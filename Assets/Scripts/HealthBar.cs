e26d66bfbcee8229a7d090de975b01f66b3ef10e
AmitSudry <amit.sudry@gmail.com>
2020-11-26 22:02:48 +0200

    Added health bar script
    Added health bar script and more minor changes in Target.cs

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image foreground;
+
+    public float updateSpeedSeconds = 0.5f;
+
+    private void Awake()
+    {
+        GetComponentInParent<Target>().OnHealthPctChanged += HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(float pct)
+    {
+        StartCoroutine(ChangeToPct(pct));
+    }
+
+    private IEnumerator ChangeToPct(float pct)
+    {
+        float preChangePct = foreground.fillAmount;
+        float elapsed = 0.0f;
+
+        while(elapsed < updateSpeedSeconds)
+        {
+            elapsed += Time.deltaTime;
+            foreground.fillAmount = Mathf.Lerp(preChangePct, pct, elapsed / updateSpeedSeconds);
+            yield return null;
+        }
+
+        foreground.fillAmount = pct;
+    }
+    /*
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+    }
+    */
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public float health = 100.0f;
+    public float maxHealth = 100.0f;
+    public event System.Action<float> OnHealthPctChanged = delegate { };
+
+    private float currHealth;
+
+    void Start()
+    {
+        currHealth = maxHealth;
+    }
 
     public void TakeDamage(float amount)
     {
-        health = health - amount;
-        if(health<=0.0f)
+        
+        currHealth = currHealth - amount;
+        if(currHealth <= 0.0f)
         {
             Die();
         }
+
+        float currHealthPct = currHealth / maxHealth;
+        OnHealthPctChanged(currHealthPct);
     }
     
     void Die()
