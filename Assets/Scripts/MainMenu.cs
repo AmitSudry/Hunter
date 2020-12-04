0e9a2a5649f14976eed2fdcf1f6c4d81d91678da
AmitSudry <amit.sudry@gmail.com>
2020-12-04 20:54:21 +0200

    Environment change support
    Now player can acutally change the loaded scene based on choise.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Image jungle;
+    public Image desert;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        if (jungle.enabled)
+            SceneManager.LoadScene("Game");
+        else if (desert.enabled)
+            SceneManager.LoadScene("GAME_TESTING");
     }
 
     public void QuitGame()
