36ec05caf65bbc08cbf4cf44618d335cb7dabc08
AmitSudry <amit.sudry@gmail.com>
2020-12-04 17:30:49 +0200

    Added UI support
    User can now change the environment based on gui.

---
+++ Assets/Scripts/NextPrevButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextPrevButton : MonoBehaviour
+{
+	public Button prev;
+	public Button next;
+
+	public Image jungle;
+	public Image desert;
+	
+	private int numOfEnv = 2;
+	private int currEnv = 0;
+
+	void Start()
+	{
+		Button btn1 = prev.GetComponent<Button>();
+		Button btn2 = next.GetComponent<Button>();
+		btn1.onClick.AddListener(TaskOnClick1);
+		btn2.onClick.AddListener(TaskOnClick2);
+	}
+
+	void TaskOnClick1()
+	{
+		if (currEnv == 0)
+			currEnv = numOfEnv - 1;
+		else
+			currEnv--;
+
+		HandleChange();
+	}
+
+	void TaskOnClick2()
+	{
+		currEnv = (currEnv + 1) % numOfEnv;
+		HandleChange();
+	}
+
+	void HandleChange()
+    {
+		if (currEnv == 0)
+		{
+			jungle.enabled = true;
+			desert.enabled = false;
+		}
+		else if (currEnv == 1)
+		{
+			jungle.enabled = false;
+			desert.enabled = true;
+		}
+	}
+}
