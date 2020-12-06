e10fe2de9732c7a2af9c51c945a3c60787fde37c
AmitSudry <amit.sudry@gmail.com>
2020-12-06 20:04:55 +0200

    Added a pause menu
    Player can now pause the game at any time and move to main menu or quit the game.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+
+    public GameObject menuUI;
+    public GameObject playerUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerUI.SetActive(true);
+        menuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        paused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        playerUI.SetActive(false);
+        menuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        paused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Resume();
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
