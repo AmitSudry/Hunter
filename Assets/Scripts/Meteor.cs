5f2f7a413f94d64ee4252b9857b15994b7cc4974
AmitSudry <amit.sudry@gmail.com>
2021-01-19 18:40:58 +0200

    Added meteor strikes
    Now every few seconds, meteors strike at random position dealing a lot of damage.

---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    private float spawnTime;
+    private float delta;
+
+    private float damage = 10000.0f; //A meteor kills instantly
+
+    public GameObject impactEffect;
+    public GameObject fireEffect;
+
+    private Rigidbody rb;
+
+    private float zForce = 0.0f;
+    private float xForce = 0.0f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        spawnTime = Time.time;
+        delta = 0.0f;
+
+        bool leftTilt = Random.Range(0, 2) == 0 ? true : false;
+        bool inwardTilt = Random.Range(0, 2) == 0 ? true : false;
+
+        if (leftTilt)
+            xForce = -4.0f;
+        else
+            xForce = 4.0f;
+
+        if (inwardTilt)
+            zForce = -4.0f;
+        else
+            zForce = 4.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta += Time.deltaTime;
+
+        if (delta >= 10.0f)
+            UnityEngine.Object.Destroy(gameObject);
+
+        transform.Rotate(0.5f, 0.5f, 1.0f);
+
+        Vector3 force = new Vector3(xForce, 0.0f, zForce);
+        rb.AddForce(force);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        rb.isKinematic = false;
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+
+        if (other.tag == "Enemy")
+        {
+            Target target = other.gameObject.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+
+        gameObject.GetComponent<Renderer>().enabled = false;
+
+        GameObject g = Instantiate(impactEffect, transform.position, Quaternion.identity);
+        UnityEngine.Object.Destroy(g, 2.0f);
+
+        GameObject f = Instantiate(fireEffect, transform.position, Quaternion.identity);
+        UnityEngine.Object.Destroy(f, 4.0f);
+
+        UnityEngine.Object.Destroy(gameObject, 4.0f);
+    }
+}
---
+++ Assets/Scripts/MeteorSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    public Transform[] meteors;
+
+    public float minx = 0.0f;
+    public float maxx = 0.0f;
+    public float minz = 0.0f;
+    public float maxz = 0.0f;
+
+    private float delta = 0.0f;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if (meteors.Length == 0)
+        {
+            Debug.Log("No referenced meteors");
+            return;
+        }
+        delta = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(delta >= 2.0f) //spawn every 5 seconds
+        {
+            delta = 0.0f;
+            spawnMeteor();
+            return;
+        }
+
+        delta += Time.deltaTime;
+    }
+
+    void spawnMeteor()
+    {
+        int index = Random.Range(0, meteors.Length);
+        Vector3 pos = new Vector3(Random.Range(minx, maxx), 300.0f, Random.Range(minz, maxz));
+
+        Instantiate(meteors[index], pos, Quaternion.identity);
+    }
+}
