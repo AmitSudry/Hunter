768ab945f816bc464ee642492b8bd883d57e3585
AmitSudry <amit.sudry@gmail.com>
2021-01-25 19:17:51 +0200

    Added zigZag behaviour
    Enemies may run away in a zigZag path.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -28,10 +28,18 @@ public class EnemyMovement : MonoBehaviour
     private float timer = 0.0f;
     public float threshold = 0.25f;
 
+    public List<Vector3> zigZagPoints = new List<Vector3>();
+    private int currZigZagPointIndex = 0;
+
+    public float offset = 2.0f;
+    public int pointsFactor = 3;
+
+    private float offsetMin;
+    private float offsetMax;
     void Start()
     {
         player = GameObject.FindGameObjectsWithTag("Player")[0];
-        if(patrolPoints==null)
+        if (patrolPoints == null)
         {
             Debug.Log("No referenced patrol points");
             return;
@@ -41,14 +49,17 @@ public class EnemyMovement : MonoBehaviour
             Debug.Log("No referenced exit points");
             return;
         }
+
+        offsetMin = offset - 0.5f;
+        offsetMax = offset + 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (followPlayer) //Player is exposed
-        {   
-            if(isActiveCreature)
+        {
+            if (isActiveCreature) //attacking
             {
                 timer += Time.deltaTime;
 
@@ -64,28 +75,62 @@ public class EnemyMovement : MonoBehaviour
                     agent.SetDestination(player.transform.position);
                     timer = 0.0f;
                 }
+
+                if (ReachedDestination())
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    SceneManager.LoadScene("Lose");
+                }
             }
-            else
+            else //running away
             {
                 if (first)
                 {
                     currExitPoint = Random.Range(0, exitPoints.Length);
                     agent.SetDestination(exitPoints[currExitPoint].position);
                     agent.speed *= 1.5f;
+
+                    if(isZigZag)
+                    {
+                        Vector2 endPath = new Vector2(exitPoints[currExitPoint].position.x, exitPoints[currExitPoint].position.z);
+                        Vector2 startPath = new Vector2(agent.transform.position.x, agent.transform.position.z);
+                        GetZigZagPath(startPath, endPath);
+
+                        reachedDest = true;
+                        Debug.Log("Count: " + zigZagPoints.Count + " index: " + currZigZagPointIndex);
+                    }
+
                     first = false;
+                }
+
+                if(isZigZag)
+                {
+                    if (reachedDest) //reached zigZag point
+                    {
+                        if(currZigZagPointIndex == zigZagPoints.Count - 1) //REACHED FINAL DESTINATION
+                        {
+                            Cursor.lockState = CursorLockMode.None;
+                            SceneManager.LoadScene("Lose");
+                        }
+                        else
+                        {
+                            agent.SetDestination(zigZagPoints[currZigZagPointIndex++]);
+                            reachedDest = false;
+                        }
+                    }
+                    else
+                    {
+                        if (ReachedDestination())
+                            reachedDest = true;
+                    }
                 }
             }
 
             if (agent.speed > 12)
                 agent.speed = 12;
 
-            if (ReachedDestination())
-            {
-                Cursor.lockState = CursorLockMode.None;
-                SceneManager.LoadScene("Lose");
-            }
         }
-        else
+        else //patroling
         {
             if (Vector3.Distance(player.transform.position, agent.transform.position) <= 20.0f) //Dont get too close to the enemy
                 followPlayer = true;
@@ -106,13 +151,13 @@ public class EnemyMovement : MonoBehaviour
             {
                 if (ReachedDestination())
                     reachedDest = true;
-            } 
+            }
         }
     }
 
     bool ReachedDestination()
     {
-        if(isBossCreature)
+        if (isBossCreature)
         {
             if (Vector3.Distance(player.transform.position, agent.transform.position) <= 10.0f) //Dont get too close to the enemy
                 return true;
@@ -130,5 +175,52 @@ public class EnemyMovement : MonoBehaviour
         }
 
         return false;
+    }
+
+    void GetZigZagPath(Vector2 v1, Vector2 v2)
+    {
+	    float dx = v2.x - v1.x;
+        float dy = v2.y - v1.y;
+
+        List<Vector2> points = new List<Vector2>();
+
+        float length = Vector2.Distance(v1, v2);
+        int numberOfPoints = pointsFactor * (int)(Mathf.Log(length, 2));
+        Vector2 direction = new Vector2(dx, dy).normalized;
+
+        Vector2 n1 = new Vector2(-dy, dx).normalized;
+        Vector2 n2 = new Vector2(dy, -dx).normalized;
+
+        float fraction = length / numberOfPoints;
+
+	    for (int i = 0; i<numberOfPoints; i++)
+	    {
+            //Debug.Log(i);
+            Vector2 dir = fraction * (i + 1) * direction;
+            Vector2 nextPoint, toAdd;
+
+            float off = Random.Range(offsetMin, offsetMax);
+
+            if (i % 2 == 0) //even
+		    {
+                nextPoint = v1 + dir; //point on the original line
+                toAdd = nextPoint + n1 * off;
+		    }
+            else
+            {
+                nextPoint = v1 + dir; //point on the original line
+                toAdd = nextPoint + n2 * off;
+            }
+
+            points.Add(toAdd);
+        }
+
+        points.Add(v2);
+
+        zigZagPoints = new List<Vector3>();
+        for (int i=0; i < numberOfPoints + 1; i++)
+        {
+            zigZagPoints.Add(new Vector3(points[i].x, 0.0f, points[i].y));
+        }
     }
 }
